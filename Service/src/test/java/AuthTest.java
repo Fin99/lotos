e184832e1fc87470fa2c1a58d518dd621755fc55
Isaev Alex <imashkaman@gmail.com>
2019-03-11 00:19:53 +0300

    feat: add test for auth

---
+++ Service/src/test/java/AuthTest.java
@@ -0,0 +1,52 @@
+import com.fin.security.Credentials;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+public class AuthTest {
+    Credentials chief = new Credentials("chief1@gmail.com", "chief");
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://192.168.0.102";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    public void testAuthenticate() {
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+
+        request.body(chief.toJson().toString());
+
+        int status = request.post("/auth/authenticate").getStatusCode();
+
+        assertEquals(status, 200);
+    }
+
+    @Test
+    public void testAuthorization() {
+        RequestSpecification request = RestAssured.given();
+        request.header("Authorization", "Bearer " + getToken());
+
+        request.get("/auth/authorization").then()
+                .body("role", equalTo("CHIEF"));
+    }
+
+    private String getToken() {
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+
+        request.body(chief.toJson().toString());
+
+        Response response = request.post("/auth/authenticate");
+
+        return response.jsonPath().get("token");
+    }
+}
