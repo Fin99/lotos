f8f50c166630458ed0141eac47a1e94bfeaae934
Isaev Alex <imashkaman@gmail.com>
2019-03-10 23:55:35 +0300

    feat: add toJson for Credentials

--- Service/src/main/java/com/fin/security/Credentials.java
+++ Service/src/main/java/com/fin/security/Credentials.java
@@ -1,14 +1,25 @@
 package com.fin.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Credentials implements Serializable {
 
     private String username;
     private String password;
+
+    public JsonObject toJson() {
+        return Json.createObjectBuilder()
+                .add("username", username)
+                .add("password", password)
+                .build();
+    }
 }
